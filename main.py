ddb35d583612d7b813fbd91637134364d491c969
Kyle Murphy <kcmurphy82@gmail.com>
2018-11-04 18:21:58 -0600

    initial templates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,11 +8,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-bl
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-class Blog(db.model):
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(255))
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime)
 
+    def __init__(self, title, body ):
+        self.title = title
+        self.body = body
+        self.created = datetime.utcnow()
 
 if __name__ == '__main__':
     app.run()
